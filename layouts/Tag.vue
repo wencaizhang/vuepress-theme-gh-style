b2a085a69ffaa2bf847e3d4ce358a1e6024eaa5f
wencaizhang <1052642137@qq.com>
2019-10-18 18:13:06 +0800

    style: update styles in tag page

--- layouts/Tag.vue
+++ layouts/Tag.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container-lg width-full px-3">
-    <ul id="default-layout">
+  <div class="container-lg width-full px-3 py-3">
+    <ul id="default-layout" class="d-flex">
       <li v-for="tag in $tag.list" :key="tag.name">
-        <router-link class="page-link" :to="tag.path">{{ tag.name }}</router-link>
+        <router-link class="page-link topic-tag topic-tag-link f6 my-1" :to="{ path: tag.path }">{{ tag.name }}</router-link>
       </li>
     </ul>
   </div>
