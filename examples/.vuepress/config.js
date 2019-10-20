6b9fe5ec04242ca7d2f1876c5a3612499985f7f8
wencaizhang <1052642137@qq.com>
2019-10-20 09:21:52 +0800

    update: update config

--- examples/.vuepress/config.js
+++ examples/.vuepress/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   title: '70 lines of vuepress blog theme',
-  plugins: ['@vuepress/last-updated'],
   theme: require.resolve('../../'),
   themeConfig: {
     avatar: 'https://gitee.com/wencaizhang_admin/images/raw/master/avatar.png',
