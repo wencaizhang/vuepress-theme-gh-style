7b48d22393ece6354738f15aded70794017b58f6
wencaizhang <1052642137@qq.com>
2019-10-21 18:13:10 +0800

    feat: 增加顶部进度条

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = (themeConfig, ctx) => {
   const config = {
     plugins: [
       ['@vuepress/last-updated'],
+      ['vuepress-plugin-nprogress'],
       ['vuepress-plugin-reading-time', {
         excludes: ['/about', '/tag/.*', '/categories/.*']
       }],
--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -17,6 +17,7 @@ export default {
 };
 </script>
 
+<style lang="stylus" src="../styles/index.styl" ></style>
 <style lang="scss">
 @import "@primer/css/index.scss";
 
