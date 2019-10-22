8d8519cc6f890daaf03a6cbbcc50bd622a2daab9
wencaizhang <1052642137@qq.com>
2019-10-22 16:47:34 +0800

    feat: 增加sidebar(TOC)

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -90,7 +90,10 @@
     </div>
     <div class="container-lg px-3 mt-3 position-relative">
       <div class="pb-3">
-        <h1>{{ $page.title }}</h1>
+        <h1>
+          <svg class="octicon octicon-repo" viewBox="0 0 12 16" version="1.1" width="12" height="32" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+          {{ $page.title }}
+        </h1>
       </div>
       <div class="Box Box--condensed d-flex flex-column flex-shrink-0">
         <div
@@ -186,6 +189,8 @@
       </div>
     </div>
 
+
+    <Toc />
     <!-- TODO -->
     <!-- <div class="container-lg px-3 mt-3 position-relative">
 
@@ -205,11 +210,12 @@
 
 
 <script>
-import RawLink from "@theme/components/RawLink";
-import BlameLink from "@theme/components/BlameLink";
+import Toc         from '@theme/components/Toc';
+import RawLink     from "@theme/components/RawLink";
+import BlameLink   from "@theme/components/BlameLink";
 import HistoryLink from "@theme/components/HistoryLink";
 export default {
-  components: { RawLink, BlameLink, HistoryLink },
+  components: { Toc, RawLink, BlameLink, HistoryLink },
   data() {
     return {
       recommendCouter: 0,
