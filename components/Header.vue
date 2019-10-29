8f16da457d3e2f2942bfd13e5f6c5d712decbfb1
wencaizhang <1052642137@qq.com>
2019-10-29 17:39:03 +0800

    feat: 将导航改为可配置项

--- components/Header.vue
+++ components/Header.vue
@@ -15,7 +15,6 @@
             d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
           />
         </svg>
-        <!-- <span>{{ $site.title }}</span> -->
       </router-link>
     </div>
 
@@ -23,14 +22,23 @@
       <div class="Header-item header-search">
         <SearchBox />
       </div>
-      <div class="Header-item">
-        <router-link class="Header-link" :to="{ path: '/'}">Home</router-link>
-      </div>
-      <div class="Header-item">
-        <router-link class="Header-link" :to="{ path: '/categories/'}">Categories</router-link>
-      </div>
-      <div class="Header-item">
-        <router-link class="Header-link" :to="{ path: '/tag/'}">Tag</router-link>
+      <div
+        v-for="item in $themeConfig.nav"
+        :key="item.link"
+        class="Header-item"
+      >
+        <a
+          v-if="item.external"
+          class="Header-link"
+          :href="item.link"
+          :target="item.target || '_blank'"
+          :rel="item.rel || 'noopener noreferrer'"
+        >{{ item.text }}</a>
+        <router-link
+          v-else
+          class="Header-link"
+          :to="{ path: item.link }"
+        >{{ item.text }}</router-link>
       </div>
     </div>
 
